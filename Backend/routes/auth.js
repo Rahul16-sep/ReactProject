d3dc86d44c4ac9d856b2c6ba9a331ee4bf65f92e
Rahul <rahuldey.333@gmail.com>
2023-07-08 12:45:34 +0530

    added authentication

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
-const User = require('../models/User')
-
+const User = require('../models/User');
 const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post('/', [
+const JWT_SECRET = 'Rahulismyname';
+
+router.post('/createUser', [
     check('email', 'Email should be an email')
                     .isEmail(),
     check('name', 'Name length should be 3 to 20 characters')
@@ -13,7 +15,7 @@ router.post('/', [
     check('password', 'Password length should be 8 to 10 characters')
                     .isLength({ min: 8, max: 10 })
 ],
-(req, res) => {
+async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -23,16 +25,42 @@ router.post('/', [
     // If no error occurs, then this
     // block of code will run
     else {
-        res.send("Successfully validated")
-        User.create({
+        try{
+            //check whether the user with this email already exist
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: "User with this email already exist"});
+        }
+        let salt = await bcrypt.genSaltSync(10);
+        let secPass = bcrypt.hashSync(req.body.password, salt);
+        user = await User.create({
             name : req.body.name,
             email : req.body.email,
-            password : req.body.password
-        }).then(user => res.json(user))
-        .catch(err => {
-            console.log(err);
-            res.json({error : 'Please enter an unique value' })
-        });
+            password : secPass
+        })
+
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        console.log(authToken);
+        // .then(user => res.json(user))
+        // .catch(err => {
+        //     console.log(err);
+        //     res.json({error : 'Please enter an unique value' })
+        // });
+
+        res.send("Successfully validated")
+        res.json({authToken});
+        
+        }catch(error){
+            console.error(error.message);
+            res.status(500).send("Some Error Occurred");
+        }
+        
     }
 })
 
